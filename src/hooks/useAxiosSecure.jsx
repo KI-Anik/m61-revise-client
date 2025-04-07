56c218e533c5f89bc66c6928cbf354aa6a9589b3
KI-Anik <iamkhairul101@gmail.com>
2025-04-07 15:18:16 +0600

    add useAxiosSecure hook for secure API requests in myApplication component

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5000',
+    withCredentials: true
+})
+const useAxiosSecure = () => {
+    const {signOutUser} = useAuth()
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(response => {
+            return response
+        }, error => {
+            console.log(' api error caught in interceptor status no -', error.status)
+
+            if (error.status == 401 || error.status == 403) {
+                signOutUser()
+                .then(()=>{
+                navigate('/signIn')
+                })
+                .catch(err=>{
+                    console.log(err.message)
+                })
+            }
+
+            return Promise.reject(error)
+        })
+    }, [])
+    return axiosInstance
+};
+
+export default useAxiosSecure;
